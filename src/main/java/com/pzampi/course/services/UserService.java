17c9714011c5d6c42df89ab6ecef7857972a24e8
pedrozampi <p.zampi@gmail.com>
2024-10-05 13:37:29 -0300

    Service layer, component registration

---
+++ src/main/java/com/pzampi/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.pzampi.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pzampi.course.entities.User;
+import com.pzampi.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
